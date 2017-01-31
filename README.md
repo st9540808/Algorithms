d1c9968ee207ea0b64efd0a59c513646a4d73d48
st9540808 <st9540808@gmail.com>
2017-02-01 05:50:54 +0800

    update

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
-# Algorithms
-
 ## Description
 This project will implement some important algorithms. <br>
 Including problem set on the textbook - Introduction to Algorithm and leetcode.
+It's meant to be concise, readable and attempt to be efficient.
 
 ## leetcode exercises
-- Sudoku Solver
+- Sudoku Solver (13msec)
--- leetcode-SudokuSolver/solveSudoku.c
+++ leetcode-SudokuSolver/solveSudoku.c
@@ -9,13 +9,13 @@ void solveSudoku(char **board, int boardRowSize, int boardColSize)
 			map[i][j] = board[i][j];
 
 	int row = 0, col = 0;
-	while (map[row][col] != '.') //initialize to the location of first blank in map[][]
+	while (map[row][col] != '.') // initialize to the location of first blank in map[][]
 	{
 		++col;
 		if (col == 9) { ++row; col = 0; if (row == 9) break;}
 	}
 
-	while (row != 9 && row != -1) //start backtracking
+	while (row != 9 && row != -1) // start backtracking
 		if (validAnsExist(board, row, col))
 		{
 			do
@@ -32,7 +32,7 @@ void solveSudoku(char **board, int boardRowSize, int boardColSize)
 				if (col == -1) { --row; col = 8; if (row == -1) break;}
 			} while (map[row][col] != '.');
 		} 
-	//end of back tracking
+	// end of back tracking
 }
 
 bool validAnsExist(char **board, int row, int col)
@@ -62,23 +62,21 @@ bool validAnsExist(char **board, int row, int col)
 
 bool isValid(char **board, char testAns, int row, int col)
 {
-	//verify row
+	// verify row
 	for (int i = 0; i < 9; ++i)
 		if (testAns != '.' && board[row][i] == testAns)
 			return false;
 
-	//verify column
+	// verify column
 	for (int i = 0; i < 9; ++i)
 		if (testAns != '.' && board[i][col] == testAns)
 			return false;
 
-	//verify cell
+	// verify cell
 	for (int i = 3 * (row / 3); i < 3 * (row / 3) + 3; ++i)
 		for (int j = 3 * (col / 3); j < 3 * (col / 3) + 3; ++j)
-		{
 			if (testAns != '.' && board[i][j] == testAns)
 				return false;
-		}
 
 	return true;
 }
