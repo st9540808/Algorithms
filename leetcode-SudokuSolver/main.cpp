4905ca9cceca1ae37fd38168eb98d40b1724dc46
st9540808 <st9540808@gmail.com>
2017-02-09 17:51:03 +0800

    add c++ version

---
+++ leetcode-SudokuSolver/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include "solveSudoku_cpp.h"
+
+char input[][10] =
+{
+	"53..7....", //0
+	"6..195...", //1
+	".98....6.", //2
+	"8...6...3", //3
+	"4..8.3..1", //4
+	"7...2...6", //5
+	".6....28.", //6
+	"...419..5", //7
+	"....8..79", //8
+};
+
+int main()
+{
+	vector<vector<char>> board(9, vector<char>(9));
+	for (int i = 0; i < 9; ++i)
+		for (int j = 0; j < 9; ++j)
+			board[i][j] = input[i][j];
+	
+	Solution solve;
+	solve.solveSudoku(board);
+
+	for (int i = 0; i < 9; ++i)
+	{
+		for (int j = 0; j < 9; ++j)
+		{
+			if (j % 3 == 0)
+				cout << " ";
+			cout << board[i][j];
+		}
+		cout << "\n";
+	}
+	
+	return 0;
+}
