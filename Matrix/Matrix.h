5a90bfa67d8a7b901aa4e6b3014f33847cd6a2f0
st9540808 <st9540808@gmail.com>
2017-03-16 21:44:12 +0800

    add overloaded operator*

--- Matrix/Matrix.h
+++ Matrix/Matrix.h
@@ -26,7 +26,8 @@ public:
 	template <typename U>
 	friend std::istream& operator>>(std::istream&, Matrix<U>&);
 
-	Matrix<T> operator+(const Matrix<T>& rhs) const;
+	Matrix<T> operator+(const Matrix<T>&) const;
+	Matrix<T> operator*(const Matrix<T>&) const;
 
 private:
 	size_type rows;
@@ -35,8 +36,10 @@ private:
 
 	inline size_type index(size_type, size_type) const;
 	inline typename std::vector<T>::iterator index_iter(size_type, size_type);
+	inline typename std::vector<T>::const_iterator index_citer(size_type, size_type) const;
 };
 
+// constructor
 template <typename T>
 Matrix<T>::Matrix(size_type inputrows, size_type inputcols, const T& initval)
 	: rows(inputrows)
@@ -79,18 +82,44 @@ Matrix<T>::index_iter(size_type row, size_type col)
 	return this->data.begin() + this->index(row, col);
 }
 
+template <typename T>
+inline typename std::vector<T>::const_iterator
+Matrix<T>::index_citer(size_type row, size_type col) const
+{
+	return this->data.cbegin() + this->index(row, col);
+}
 
 template <typename T>
 Matrix<T> Matrix<T>::operator+(const Matrix<T>& rhs) const
 {
 	if (this->rows != rhs.rows || this->cols != rhs.cols)
-		throw std::out_of_range("wrong dimension in operator+");
+		throw std::domain_error("wrong dimension in operator+");
 
 	Matrix<T> result(this->rows, this->cols);
 	for (auto resultIter = result.data.begin(), thisIter = this->data.begin(),
 	          rhsIter = rhs.data.begin(); resultIter != result.data.end();
 	        ++resultIter, ++thisIter, ++rhsIter)
 		*resultIter = *thisIter + *rhsIter;
+
+	return result;
+}
+
+template <typename T>
+Matrix<T> Matrix<T>::operator*(const Matrix<T>& rhs) const
+{
+	if (this->cols != rhs.rows)
+		throw std::domain_error("wrong dimension in operator*");
+		
+	Matrix<int> result(this->rows, rhs.cols);
+
+	for (size_type i = 0; i < this->rows; ++i)
+		for (size_type j = 0; j < rhs.cols; ++j) {
+			T sum = T();
+			for (size_type k = 0; k < this->rows; ++k)
+				if (*this->index_citer(i, k) != 0 || *rhs.index_citer(k, j) != 0)
+					sum += *this->index_citer(i, k) * *rhs.index_citer(k, j);
+			*result.index_iter(i, j) = sum;
+		}
 
 	return result;
 }
